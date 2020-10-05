b57b97ad18a84e27a67b9a552ac00e73500bf697
TrieKai <trie55432@gmail.com>
2020-10-05 18:28:01 +0800

    Fix signup user without name error

--- api/models/User.go
+++ api/models/User.go
@@ -15,7 +15,7 @@ import (
 type User struct {
 	ID        uint32    `gorm:"primary_key;auto_increment" json:"id"`
 	UserID    string    `gorm:"size:255;not null;unique" json:"userId"`
-	Name      string    `gorm:"size:255;not null;unique" json:"name"`
+	Name      string    `gorm:"size:255" json:"name"`
 	Email     string    `gorm:"size:100;not null;unique" json:"email"`
 	Password  string    `gorm:"size:100;not null;" json:"password"`
 	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
@@ -82,9 +82,6 @@ func (u *User) Validate(action string) error {
 	default:
 		if u.UserID == "" {
 			return errors.New("Required UserID")
-		}
-		if u.Name == "" {
-			return errors.New("Required Name")
 		}
 		if u.Password == "" {
 			return errors.New("Required Password")
